41b13003486aa6466ebcf098f6c22f4683286270
abyanfalah <abyanf410@gmail.com>
2023-06-25 07:23:19 +0700

    add transition

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ createApp({
 		]);
 		const emailAddress = ref('');
 		const validEmail = ref(true);
-		const signupSuccess = ref(false);
+		const signupSuccess = ref(true);
 
 
 		const validateEmail = (email) => {
